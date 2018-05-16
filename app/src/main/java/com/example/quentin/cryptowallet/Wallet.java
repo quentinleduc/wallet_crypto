8963b37c6d30be1f59dbc67bff21143479adc77c
Le Duc Quentin <quentinleduc03@gmail.com>
2018-05-16 15:56:55 +0200

    Ajout Wallet

---
+++ app/src/main/java/com/example/quentin/cryptowallet/Wallet.java
@@ -0,0 +1,13 @@
+package com.example.quentin.cryptowallet;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Wallet extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wallet);
+    }
+}
